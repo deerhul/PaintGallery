d2ab03c3c8cda8eb44d310e4b28613f6e72f7a62
daryl sangalang <dee.yess@live.com>
2018-05-27 23:33:11 +1000

    new effects for index page. -store data in database for easier editing etc.
    about page changes. needs cleaning up a bit

--- PaintGallery/App_Start/BundleConfig.cs
+++ PaintGallery/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace PaintGallery
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Custom.css"));
+
         }
     }
 }
